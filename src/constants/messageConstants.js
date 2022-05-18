a389b33282eba30f09110f52cc226a0434000590
pavan <pavankumar.y.c@greenwayhealth.com>
2022-05-18 13:08:56 +0530

    resolved merge conflicts

--- src/constants/messageConstants.js
+++ src/constants/messageConstants.js
@@ -1,9 +1,5 @@
-exports.noUserNamePassword = 'You must send the username and the password';
-exports.wrongUserName = 'Wrong UserName';
-exports.wrongPassword = 'Wrong Password';
-<<<<<<< HEAD
-exports.alreadyTaken = 'Already Taken';
-=======
-exports.alreadyTaken = 'Alreadytaken';
->>>>>>> e152a5a28389f8fcb245793a2ee39f86458d7aeb
-exports.available = 'Available';
+exports.noUserNamePassword = "You must send the username and the password";
+exports.wrongUserName = "Wrong UserName";
+exports.wrongPassword = "Wrong Password";
+exports.alreadyTaken = "Already Taken";
+exports.available = "Available";
